e62ff1366095030f3be55e8f51b2917c9b059e5c
David Fischer <davidfisc@hotmail.com>
2020-07-23 08:11:30 -0600

    homework done

--- index.js
+++ index.js
@@ -6,40 +6,11 @@
 
 // Dependencies
 const http = require('http');
-const https = require('https')
 const url = require('url');
-const { StringDecoder } = require('string_decoder');
-const stringDecoded = require('string_decoder').StringDecoder;
-const config = require('./config.js');
-const fs = require('fs');
 
 
 // start the server and have it listen on port 3000
-const httpServer = http.createServer((req, res)=>{
-	unifiedServer(req, res)
-})
-httpServer.listen(config.httpPort, ()=>{
-	console.log('listening on port ' + config.httpPort + ' in ' + config.envName + ' mode.')
-})
-
-// https server setup and start
-const httpsServerOptions = {
-	'key': fs.readFileSync('./https/key.pem'),
-	'certificate': fs.readFileSync('./https/certificate.pem')
-}
-const httpsServer = https.createServer(httpsServerOptions, (req, res)=>{
-	unifiedServer(req, res)
-})
-
-httpsServer.listen(config.httpsPort, () => {
-  console.log(
-    "listening on port " + config.httpsPort + " in " + config.envName + " mode."
-  );
-});
-
-
-// All the server logic
-var unifiedServer = function (req, res) {
+const server = http.createServer((req, res)=>{
 		
 	// parse url
 	const parsedUrl = url.parse(req.url, true);
@@ -47,65 +18,34 @@ var unifiedServer = function (req, res) {
 	// get the path
 	const path = parsedUrl.pathname;
 	const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+	const chosenHandler = router.hasOwnProperty(trimmedPath)
+        ? router[trimmedPath]
+        : handlers.notFound;
 
-	// get the request method
-	const method = req.method.toLowerCase();
+	chosenHandler(function (payloadJson) {
 
-	// get the query string as an object
-	const queryStringObject = parsedUrl.query;
+		res.setHeader("Content-Type", "application/json");
 
-	// get headers
-	const headers = req.headers;
+		res.end(JSON.stringify(payloadJson));
 
-	// get and parse payload if available
-	const decoder = new StringDecoder('utf-8');
-	var buffer = '';
-	req.on('data', function () {
-		buffer += decoder.write(data);
-	});
-	req.on('end', function () {
-		buffer += decoder.end();
-
-		// choose the handler this request should go to, if not found use notFound handler
-		console.log(trimmedPath)
-		const chosenHandler = router.hasOwnProperty(trimmedPath) ? router[trimmedPath] : handlers.notFound;
-		const data = {
-			'trimmedPath': trimmedPath,
-			'queryStringObject': queryStringObject,
-			'method': method,
-			'headers': headers,
-			'payload': buffer
-		}
-		// Route the request to handler
-		chosenHandler(data, function(statusCode, payload){
-			// default status code
-			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-			payload = typeof(payload) == 'object' ? payload : {};
-
-
-
-			// use payload from handler or default to default payload
-			let payloadString = JSON.stringify(payload)
-			res.setHeader('Content-Type', 'application/json')
-			res.writeHead(statusCode);
-			res.end(payloadString);
-			
-			console.log(`Returning this STATUSCODE: ${statusCode}...PAYLOAD: ${payloadString}`)
-		})
-	});
-}
+		console.log(`REQUEST route: ${trimmedPath}`);
+    });
+})
+server.listen(3000, ()=>{
+	console.log('listening on port 3000')
+})
 
 // Define the handlers
 const handlers = {
-	ping: (data, callback)=>{
-		callback(200)
+	hello: (callback)=>{
+		callback({"message": "Welcome to my simple api"})
 	},
-	notFound: (data, callback)=>{
+	notFound: (callback)=>{
 		callback(404)
 	}
 }
 
 // Define a request router
 const router = {
-	'ping' : handlers.ping
+	'hello' : handlers.hello
 }
